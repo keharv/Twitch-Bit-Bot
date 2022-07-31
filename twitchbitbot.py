83ea9a3681d81f442d10c815ccf172b94687c3e0
Keith Harvey <keharv@iu.edu>
2022-07-31 00:17:52 -0400

    fix fake cheer bug

--- twitchbitbot.py
+++ twitchbitbot.py
@@ -10,8 +10,8 @@ from threading import Semaphore
 
 
 #enter access token here
-#modify custom emote on line 43
-access = 'twitch_access_token'
+#add custom cheer emote to list on line 37
+access = ''
  
 
 
@@ -25,7 +25,7 @@ class Bot(commands.Bot):
         # prefix can be a callable, which returns a list of strings or a string...
         # initial_channels can also be a callable which returns a list of strings...
         self.lock = asyncio.Lock()
-        super().__init__(token=access, prefix='?', initial_channels=['your_channel'])
+        super().__init__(token=access, prefix='?', initial_channels=['keith282'])
  
     async def event_ready(self):
         # Notify us when everything is ready!
@@ -34,43 +34,25 @@ class Bot(commands.Bot):
         print(f'User id is | {self.user_id}')
  
     async def event_message(self, message):
-        #this is called when a message is sent to the channel
-        
+        cheer_beginning = ['cheer', 'biblethump', 'cheerwhal', 'corgo', 'uni', 'showlove', 'party', 'seemsgood', 'pride', 'kappa', 'frankerz', 'heyguys', 'dansgame', 'elegiggle', 'trihard', 'kreygasm', '4head', 'swiftrage', 'notlikethis', 'failfish', 'vohiyo', 'pjsalt', 'mrdestructoid', 'bday', 'ripcheer', 'shamrock']
+
         #get the bit donations, and add up the amount of bits donated in the message
         words = message.content.lower()
-        m = sum([int(j) for i in re.finditer(r'\bcheer(\d+)\b', words) for j in i.groups() if j is not None])
+        words = words.replace('\n', ' ')
+        while("  " in words):
+            words = words.replace("  ", " ")
+        words = words.split(' ')
+        cheer = 0
+        for word in words:
+            word_no_numbers = re.sub(r'\d+', '', word)
+            if word_no_numbers in cheer_beginning:
 
-        custom_emote = "usercheer"
-        m += sum([int(j) for i in re.finditer(r'\b' + custom_emote + r'(\d+)\b', words) for j in i.groups() if j is not None])
-
-        m += sum([int(j) for i in re.finditer(r'\bbiblethump(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bcheerwhal(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bcorgo(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\buni(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bshowlove(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bparty(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bseemsgood(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bpride(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bkappa(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bfrankerz(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bheyguys(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bdansgame(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\belegiggle(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\btrihard(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bkreygasm(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\b4head(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bswiftrage(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bnotlikethis(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bfailfish(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bvohiyo(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bpjsalt(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bmrdestructoid(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bbday(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bripcheer(\d+)\b', words) for j in i.groups() if j is not None])
-        m += sum([int(j) for i in re.finditer(r'\bshamrock(\d+)\b', words) for j in i.groups() if j is not None])
-
-        #send to function that does things on bit donations
-        await self.bitsDonated(m)
+                number_from_word = re.findall(r'\d+', word)
+                if(len(number_from_word) == 0):
+                    continue
+                cheer_amount += int(number_from_word[0]) 
+ 
+        await self.bitsDonated(cheer)
  
         # Since we have commands and are overriding the default `event_message`
         # We must let the bot know we want to handle and invoke our commands...
